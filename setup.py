2dd2d7bcf6481ffc8a4c8904efa092a1a99fa87a
SeanCCarter <seancarter26@gmail.com>
2016-10-27 14:53:49 -0400

    Changing package name to audioppm, since pyppm is taken.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'PyPPM',
-  packages = ['PyPPM'], # this must be the same as the name above
+  name = 'audioppm',
+  packages = ['audioppm'], # this must be the same as the name above
   version = '0.1',
   description = 'Output PPM from the audio jack',
   author = 'Sean Carter and Jacob Regenstein',
