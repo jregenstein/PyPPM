75a1821b7464e967adace59fb18e85d595561ad7
SeanCCarter <seancarter26@gmail.com>
2016-10-27 14:32:25 -0400

    Added setup files, and MIT license.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'PyPPM',
+  packages = ['PyPPM'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Output PPM from the audio jack',
+  author = 'Sean Carter and Jacob Regenstein',
+  author_email = 'peterldowns@gmail.com',
+  url = 'https://github.com/jregenstein/PyPPM', # use the URL to the github repo
+  download_url = 'https://github.com/jregenstein/PyPPM/tarball/0.1', # I'll explain this in a second
+  keywords = ['ppm', 'audio', 'buddybox'], # arbitrary keywords
+  classifiers = [],
+)
